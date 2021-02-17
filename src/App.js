4447ae4bfaee66c63ebbe42731f335e9bb933aad
harsh-vish14 <harshkv9322@gmail.com>
2021-02-17 22:28:47 +0530

    Clock react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
 
 function App() {
+  var [state, setState] = useState('TIME');
+  var updateTime = () => {
+    var time = new Date().toLocaleTimeString()
+    setState(time);
+  }
+  setInterval(updateTime, 1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="clockTime">{state}</h1>
     </div>
   );
 }
